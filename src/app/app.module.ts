24e664966e81489aaeb2eaab720f9dc39049007b
Alexandre Peyrat <alexandrepeyrat@imac-de-alexandre.home>
2019-02-27 23:39:35 +0100

    change todoState #2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,5 @@
+import { TodosEffects } from './shared/store/todos.effects';
+import { reducers } from './shared/store/index';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ApiService } from './shared/services/api.service';
 import { HttpModule } from '@angular/http'
@@ -9,6 +11,11 @@ import { AppComponent } from './app.component';
 import { TodoListComponent } from './todo-list/todo-list.component';
 import { FormsModule } from '@angular/forms';
 import { CoreModule } from '@angular/flex-layout';
+import { StoreModule } from '../../node_modules/@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { EffectsModule } from '../../node_modules/@ngrx/effects';
+import { RouterModule } from '../../node_modules/@angular/router';
+import { StoreRouterConnectingModule } from '../../node_modules/@ngrx/router-store';
 
 @NgModule({
   declarations: [
@@ -21,7 +28,29 @@ import { CoreModule } from '@angular/flex-layout';
     FormsModule,
     LayoutModule,
     CoreModule,
-    HttpModule
+    HttpModule,
+
+    StoreModule.forRoot(reducers), 
+     StoreDevtoolsModule.instrument({
+      name: 'to do'
+    }),
+    EffectsModule.forRoot([TodosEffects]),
+    RouterModule.forRoot([
+      {
+        path: 'todo', component: TodoListComponent
+      },
+      {
+        path: '', redirectTo: 'todo', pathMatch: 'full',
+      },
+      {
+        path: 'todo/:id', component: TodoListComponent
+      }
+    ]),
+    StoreRouterConnectingModule.forRoot({
+      stateKey: 'router'
+    })
+    
+  
   ],
   providers: [ApiService],
   bootstrap: [AppComponent]
--- src/app/todo-list/todo-list.component.ts
+++ src/app/todo-list/todo-list.component.ts
@@ -1,7 +1,11 @@
+import { todoListSelector, selectedTodoSelector } from './../shared/store/selector';
 import { ApiService } from './../shared/services/api.service';
 import { Todo } from './../shared/models/todo.model';
 import { Observable } from 'rxjs';
 import { Component, OnInit } from '@angular/core';
+import * as todosAction from '../shared/store/todos.action'
+import { Store, select } from '../../../node_modules/@ngrx/store';
+import { State } from '../shared/store';
 
 @Component({
   selector: 'app-todo-list',
@@ -10,11 +14,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TodoListComponent implements OnInit {
 
-  public todos$: Observable<Todo[]> = this.apiService.todos$;
+  public todos$: Observable<Todo[]> = this.store.pipe(select(todoListSelector));
   
-  constructor(private apiService: ApiService) { }
+  public selectedTodo$: Observable<Todo> = this.store.pipe(select(selectedTodoSelector));
+  constructor(private apiService: ApiService, private store: Store<State>) { }
 
   ngOnInit() {
+    this.store.dispatch(new todosAction.FetchTodo());
   }
+
+  public toggleTodo(index: number) {
+    this.store.dispatch(new todosAction.ToggleTodo(index));
+    }
 
 }
